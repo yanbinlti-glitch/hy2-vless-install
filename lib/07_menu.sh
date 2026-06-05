da3fa33d8926e5e0d1049749578928f46cb0a69c
哆啦？ <yan.bin.lti@gmail.com>
2026-06-05 03:57:07 +0000

    微调

--- lib/07_menu.sh
+++ lib/07_menu.sh
@@ -142,15 +142,15 @@ main_realtime_status_panel() {
         warp_iface="未检测到"
     fi
 
-    echo -e 
     echo -e " ${LIGHT_CYAN}实时状态面板${PLAIN}"
-    echo -e 
-    echo -e " ${LIGHT_YELLOW}Sing-box内核:${PLAIN} ${sb_ver}    ${LIGHT_YELLOW}最新正式版:${PLAIN} ${sb_latest}"    ${LIGHT_YELLOW}Sing-box状态:${PLAIN} ${svc_text}"
-    echo -e " ${LIGHT_YELLOW}系统:${PLAIN} ${os_name}    ${LIGHT_YELLOW}内核:${PLAIN} ${kernel}    
+     red "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    echo -e " ${LIGHT_YELLOW}Sing-box内核:${PLAIN} ${sb_ver}    ${LIGHT_YELLOW}最新正式版:${PLAIN} ${sb_latest}    ${LIGHT_YELLOW}Sing-box状态:${PLAIN} ${svc_text}"
+    echo -e " ${LIGHT_YELLOW}系统:${PLAIN} ${os_name}    ${LIGHT_YELLOW}内核:${PLAIN} ${kernel} "   
     echo -e " ${LIGHT_YELLOW}BBR算法:${PLAIN} ${bbr}     ${LIGHT_YELLOW}架构:${PLAIN} ${arch}    ${LIGHT_YELLOW}虚拟化:${PLAIN} ${virt}"
     echo -e " ${LIGHT_YELLOW}本机IPv4:${PLAIN} ${ipv4}"
     echo -e " ${LIGHT_YELLOW}本机IPv6:${PLAIN} ${ipv6}"    
     echo -e " ${LIGHT_YELLOW}WARP接口:${PLAIN} ${warp_iface}"
+     red "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
     echo ""
     main_status_show_node_info
     echo ""
